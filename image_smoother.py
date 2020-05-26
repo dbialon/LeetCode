4d01fd87e93ab98f7b9e95bbee0bf08bc9d7545e
Dawid Bialon <dbialon79@outlook.com>
2020-05-26 09:01:46 +0100

    Comments added

--- image_smoother.py
+++ image_smoother.py
@@ -38,17 +38,19 @@ class Solution:
         # define max_X and max_Y
         max_X, max_Y = len(M) - 1, len(M[0]) - 1
 
-        # iterate over pixel in the original image
+        # iterate over pixels in the original image
         for x, _ in enumerate(M):
             for y, _ in enumerate(M[x]):
                 # maps of neighbour pixels for x and y
                 map_x = (x - 1, x, x + 1)
                 map_y = (y - 1, y, y + 1)
+                # count valid pixels (not outside the image)
+                # sum of pixel values
                 count, pixel_sum = 0, 0
                 # iterate over neighbours of each pixel
                 for p in map_x:
                     for q in map_y:
-                        # eliminate pixels 'outside' of image
+                        # eliminate pixels 'outside' the image
                         if 0 <= p <= max_X and 0 <= q <= max_Y:
                             pixel_sum += M[p][q]
                             count += 1
@@ -57,6 +59,11 @@ class Solution:
                 N[x][y] = pixel_sum // count 
 
         return N
+
+
+####################
+### TESTING ONLY ###
+####################
 
 # M = [[1,1,1],[1,0,1],[1,1,1]]
 # M = [[1]]
