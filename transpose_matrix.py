7c4eda45486c637fdf70d6381d8a2975157f432c
Dawid Bialon <dbialon79@outlook.com>
2020-06-01 14:32:43 +0100

    Main function completed

--- transpose_matrix.py
+++ transpose_matrix.py
@@ -11,12 +11,26 @@ from typing import List
 class Solution:
     def transpose(self, A: List[List[int]]) -> List[List[int]]:
         B = []
+        i, n = 0, len(A[0])
 
+        while i < n:
+            B.append([])
+            for idx, _ in enumerate(A):
+                B[i].append(A[idx][i])
+            i += 1
 
         return B
 
-A = [[1,2,3],[4,5,6],[7,8,9]]
-print(A)
 
+####################
+### TESTING ONLY ###
+####################
+
+A = [[1,2,3],[4,5,6],[7,8,9]]
+# A = [[1,2,3],[4,5,6]]
+# A = [[1,2,3]]
+# A = [[1]]
+
+print("A:", A)
 solution = Solution().transpose(A)
 print("B:", solution)
