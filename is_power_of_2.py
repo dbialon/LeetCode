4644df4fd667a088bc3671a2d344c05118dac774
Dawid Bialon <dbialon79@outlook.com>
2020-06-12 11:27:40 +0100

    Changed dict to set

--- is_power_of_2.py
+++ is_power_of_2.py
@@ -1,12 +1,16 @@
 class Solution:
-    powers_of_2 = {2**n: True for n in range(64)}
+    powers_of_2 = {2**n for n in range(64)}
     def isPowerOfTwo(self, n: int) -> bool:
         if n in self.powers_of_2:
             return True
         else:
             return False
 
+
+####################
+### TESTING ONLY ###
+####################
+
 solution = Solution()
 for number in range(17):
     print(number, solution.isPowerOfTwo(number))
-print(solution.powers_of_2)
