2803765f5d1f672340c36b2d3eba79871fc24a25
Dawid Bialon <dbialon79@outlook.com>
2020-06-22 10:37:32 +0100

    Initial commit

---
+++ single_number_ii.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/
+# 542/week-4-june-22nd-june-28th/3368/
+
+# Given a non-empty array of integers, every element appears three times
+# except for one, which appears exactly once. Find that single one.
+# Note:
+# Your algorithm should have a linear runtime complexity. Could you
+# implement it without using extra memory?
+
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        i = 1
+        while i < len(nums):
+            if nums[i] == nums[i-1]:
+                i += 3
+            else:
+                return nums[i-1]
+        else:
+            return nums[-1]
+
+
+####################
+### TESTING ONLY ###
+####################
+
+# TODO reduce runtime to linear time
+
+# nums = [2,2,3,2]
+nums = [0,1,0,1,0,1,99]
+
+solution = Solution()
+print(solution.singleNumber(nums))
